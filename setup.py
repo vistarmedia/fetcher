38372a4f3c1d0dcf5cbc9b884f21d8fc54e87567
Chris McBride <chris@vistarmedia.com>
2013-10-30 17:05:01 -0400

    remove requirements.txt

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-from pip.req import parse_requirements
 
-install_reqs = parse_requirements('requirements.txt')
-reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
   name='fetcher',
   version='0.1',
-  install_requires=reqs,
+  install_requires=['pycurl==7.19'],
   packages=['fetcher'])
